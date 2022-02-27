7cb598e06cbc044efec892bdea72d95f14c5faeb
thyagodias <thyagojuniorrochadias@gmail.com>
2022-02-27 19:03:09 -0300

    feat: added get user todos routes

--- desafio-01-conceitos/src/index.js
+++ desafio-01-conceitos/src/index.js
@@ -11,8 +11,13 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const isUser = users.some(user => user.username === request.headers.username)
+
+  if (isUser) return next()
+
+  response.status(404).json({ error: "User not found" })
 }
+
 
 function createUser(payload) {
   return {
@@ -23,15 +28,21 @@ function createUser(payload) {
   }
 }
 
+function findUser(key = "id", value) {
+  return users.find(user => user[key] === value)
+}
+
 app.post('/users', (request, response) => {
   const newUser = createUser(request.body)
   users.push(newUser)
 
-  response.json(newUser)
+  response.status(201).json(newUser)
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const user = findUser("username", request.headers.username)
+
+  response.json(user.todos)
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
